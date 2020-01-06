cb920fd81c41a8de99c6a358a51e6b050dce4e92
atchiko <59562280+atchiko@users.noreply.github.com>
2020-01-06 18:50:17 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 //JSX
-React.render(<h1>Hello Achiko</h1>, document.getElementById("root"))
+ReactDOM.render(<h1>Hello Achiko</h1>, document.getElementById("root"))
